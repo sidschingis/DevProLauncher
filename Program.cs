dd80f4b313f69bc3691431b1e97d425ae8d070eb
sidschingis <sijing@gmx.net>
2015-02-02 01:35:12 +0100

    version ++

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace DevProLauncher
 {
     static class Program
     {
-        public const string Version = "210000";
+        public const string Version = "210010";
         public static Configuration Config;
         public static LanguageManager LanguageManager;
         public static ChatClient ChatServer;

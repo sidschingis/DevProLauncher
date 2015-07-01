80acc5f27a62f0f63ac366f79d18060b95c59df5
Tic-Tac-Toc <luigixd@hotmail.fr>
2015-07-02 01:59:03 +0200

    Add Bot Tag in Pm
    http://puu.sh/iK3TU/cb4a8fed86.png

--- Helpers/ChatHelper.cs
+++ Helpers/ChatHelper.cs
@@ -122,9 +122,12 @@ namespace DevProLauncher.Helpers
                         case 4:
                             text = "SMod";
                             break;
-                        case 99:
-                            text = "Dev";
+                        case 98:
+                            text = "Bot";
                             break;
+                     /*   case 99:
+                            text = "Dev";
+                            break; */
                     }
                     LogText += text;
                     LogText += "]";

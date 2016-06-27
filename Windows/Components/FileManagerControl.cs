4ef86582a85b2f87404a417ef620392404b071b1
SuperAndroid17 <sean7-6@hotmail.com>
2016-06-27 06:27:30 -0500

    Open Deck from File Manager

--- Windows/Components/FileManagerControl.cs
+++ Windows/Components/FileManagerControl.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Globalization;
 using System.Text;
 using System.Windows.Forms;
@@ -69,7 +69,7 @@ namespace DevProLauncher.Windows.Components
         {
             if (Name == "Decks")
             {
-                LauncherHelper.GenerateConfig();
+                LauncherHelper.GenerateConfig(2, FileList.SelectedItem.ToString());
                 LauncherHelper.RunGame("-d");    
             } else
             {
@@ -90,7 +90,7 @@ namespace DevProLauncher.Windows.Components
                     MessageBox.Show("Selected file does not exist!");
                     return;
                 }
-                LauncherHelper.GenerateConfig(true, FileList.SelectedItem + ".yrp");
+                LauncherHelper.GenerateConfig(0, FileList.SelectedItem + ".yrp");
                 LauncherHelper.RunGame("-r");
             }
             

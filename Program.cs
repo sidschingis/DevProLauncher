58eaabbe414e71038687b47a4314f3315ec6dc6d
SuperAndroid17 <sean7-6@hotmail.com>
2016-09-15 13:49:48 -0500

    Version++

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace DevProLauncher
 {
     static class Program
     {
-        public const string Version = "210480";
+        public const string Version = "210481";
         public static Configuration Config;
         public static LanguageManager LanguageManager;
         public static ChatClient ChatServer;
